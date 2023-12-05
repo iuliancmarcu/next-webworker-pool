022d0ff6d2b12c51f2a9a16e716499394912ff51
Iulian-Constantin Marcu <iulian-constantin.marcu@popsa.com>
2023-12-05 10:51:35 +0000

    types: change export name to match types

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'next-webworker-pool',
-            fileName: 'next-webworker-pool',
+            name: 'index',
+            fileName: 'index',
         },
     },
     plugins: [dts()],
